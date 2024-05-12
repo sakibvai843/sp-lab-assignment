36241cbf72a54910efe800eb7e2ed70e0013bac6
sakibvai843 <sanju84143@gmail.com>
2024-05-12 21:10:13 +0600

    Create question_4.c

---
+++ question_4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+//Write a program to print all prime numbers between ‘a’ to ‘n’ (a for Lower and n for upper number)
+
+int main() {
+    int a, n, i, j, isPrime;
+    printf("Enter the lower bound 'a': ");
+    scanf("%d", &a);
+    printf("Enter the upper bound 'n': ");
+    scanf("%d", &n);
+
+    printf("Prime numbers between %d and %d are: ", a, n);
+
+    for (i = a; i <= n; i++) {
+        isPrime = 1;
+        if (i == 1) isPrime = 0; 
+        else {
+            for (j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = 0;
+                    break; 
+                }
+            }
+        }
+
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n"); 
+
+    return 0;
+}
